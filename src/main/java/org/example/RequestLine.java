e4745e359eb0fa351d79facc5eb3271f0d87cb25
illyoung22 <illyoung22@hanmail.net>
2023-09-21 19:01:58 +0900

    나만의 MVC 프레임워크 만들기 강의 - 계산기 구현

--- src/main/java/org/example/RequestLine.java
+++ src/main/java/org/example/RequestLine.java
@@ -2,27 +2,41 @@ package org.example;
 
 import java.util.Objects;
 
+// GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
 public class RequestLine {
-    private final String method; //GET
-    private final String urlPath; // /calculate?operand1=11&operator=*&operand2=55
-    private String queryString;
+    private final String method; // GET
+
+    private final String urlPath; // /calculate
+
+    private QueryStrings queryStrings; // operand1=11&operator=*&operand2=55
+
+    public RequestLine(String method, String urlPath, String queryStrings) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryStrings = new QueryStrings(queryStrings);
+    }
 
     public RequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
         this.method = tokens[0];
-        String[] urlTokens = tokens[1].split("\\?");
-        this.urlPath = urlTokens[0];
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
 
-        if (urlTokens.length == 2) {
-            this.queryString = urlTokens[1];
+        if (urlPathTokens.length == 2) {
+            this.queryStrings = new QueryStrings(urlPathTokens[1]);
         }
-
     }
 
-    public RequestLine(String method, String urlPath, String queryString) {
-        this.method = method;
-        this.urlPath = urlPath;
-        this.queryString = queryString;
+    public boolean isGetRequest() {
+        return "GET".equals(this.method);
+    }
+
+    public boolean matchPath(String requestPath) {
+        return urlPath.equals(requestPath);
+    }
+
+    public QueryStrings getQueryStrings() {
+        return this.queryStrings;
     }
 
     @Override
@@ -30,11 +44,11 @@ public class RequestLine {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RequestLine that = (RequestLine) o;
-        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryStrings, that.queryStrings);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(method, urlPath, queryString);
+        return Objects.hash(method, urlPath, queryStrings);
     }
 }
