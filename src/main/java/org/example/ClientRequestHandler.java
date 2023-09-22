5713ed37851e9815f0b548f398ce1630102c61e8
illyoung22 <illyoung22@hanmail.net>
2023-09-22 13:15:59 +0900

    나만의 MVC 프레임워크 만들기 강의 - 계산기 구현 thread 생성

---
+++ src/main/java/org/example/ClientRequestHandler.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.example.calculator.Calculator;
+import org.example.calculator.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ClientRequestHandler implements Runnable{
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+    private final Socket clientSocekt;
+
+    public ClientRequestHandler(Socket clientSocekt) {
+        this.clientSocekt = clientSocekt;
+    }
+
+    @Override
+    public void run() {
+        //Step2 - 사용자 요청이 들어올 때마다 THREAD를 새로 생성하여 사용자 요청을 처리하도록 한다.
+        logger.info("[ClientRequestHandler] new client {} started", Thread.currentThread().getName());
+        try (InputStream in = clientSocekt.getInputStream(); OutputStream out = clientSocekt.getOutputStream()){
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            HttpRequest httpRequest = new HttpRequest(br);
+            if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
+                QueryStrings queryStrings = httpRequest.getQueryStrings();
+                int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                String operator = queryStrings.getValue("operator");
+                int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                int result = Calculator.calculate(new PositiveNumber(operand1),operator, new PositiveNumber(operand2));
+                byte[] body  = String.valueOf(result).getBytes();
+
+                HttpResponse response = new HttpResponse(dos);
+                response.response200Header("application/json",body.length);
+                response.responseBody(body);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
